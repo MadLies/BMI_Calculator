f17b3656164b4fbc833d3d4a3e5eb29b7823ad4a
MadLies <jwilchesc@unal.edu.co>
2023-01-16 05:31:05 -0500

    Final Version

--- BMI/BMI/MVVM/Models/BMI.cs
+++ BMI/BMI/MVVM/Models/BMI.cs
@@ -1,4 +1,4 @@
-﻿using Kotlin.Contracts;
+﻿
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
--- BMI/BMI/MVVM/ViewModels/BMIViewModel.cs
+++ BMI/BMI/MVVM/ViewModels/BMIViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Maui.Animations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BMI.MVVM.ViewModels
 {
@@ -10,14 +12,38 @@ namespace BMI.MVVM.ViewModels
 
     {
         public Models.BMI BMI { get; set; }
-
+        public ICommand ReduceHCommand { get; set; }
+        public ICommand IncreaseHCommand { get; set; }
+        public ICommand ReduceWCommand { get; set; }
+        public ICommand IncreaseWCommand { get; set; }
         public BMIViewModel()
-        { 
+        {
+
+
             BMI = new Models.BMI();
-            BMI.Height= 100;
-            BMI.Weight= 100;
+            BMI.Height = 0;
+            BMI.Weight = 0;
+            ReduceHCommand = new Command(() =>
+            {
+                BMI.Height--;
+            });
+            IncreaseHCommand = new Command(() =>
+            {
+                BMI.Height++;
+            });
+            ReduceWCommand = new Command(() =>
+            {
+                BMI.Weight--;
+            });
+            IncreaseWCommand = new Command(() =>
+            {
+                BMI.Weight++;
+            });
+
 
         }
+
+    
 
     }
 }
